52de7eb10d6d8409a86660cddf9c08f4a0bd4919
Paul Thomas <paulthomas2107@gmail.com>
2022-10-26 12:29:50 +0100

    more...

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const log = console.log;
 
 const pokeAPIBaseURL = 'https://pokeapi.co/api/v2/pokemon/';
-const game = document.getElementById('game')
-
+const game = document.getElementById('game');
 
 const colors = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
 };
 
 const loadPokemom = async () => {
@@ -29,43 +28,56 @@ const loadPokemom = async () => {
     randomIds.add(randomNumber);
   }
 
-  const pokePromises = [...randomIds].map(id => fetch(pokeAPIBaseURL + id))
-  const responses = await Promise.all(pokePromises)
+  const pokePromises = [...randomIds].map((id) => fetch(pokeAPIBaseURL + id));
+  const responses = await Promise.all(pokePromises);
 
-  return await Promise.all(responses.map(res => res.json()))
+  return await Promise.all(responses.map((res) => res.json()));
 };
 
-/*
-<div class='front'>
-      </div>
-      */
-
 const displayPokemon = (pokemon) => {
-  pokemon.sort(_ => Math.random() - 0.5)
-  const pokemonHTML = pokemon.map(pokemon => {
-    const type = pokemon.types[0]?.type?.name || 'normal'
-    const color = colors[type]
-    return `
-    <div class='card'>
-      
-      <div class='back rotate' style='background-color: ${color};'>
+  pokemon.sort((_) => Math.random() - 0.5);
+  const pokemonHTML = pokemon
+    .map((pokemon) => {
+      const type = pokemon.types[0]?.type?.name || 'normal';
+      const color = colors[type];
+      return `
+    <div class='card' style='background-color: ${color};' onclick='clickCard(event)' data-pokename='${pokemon.name}'>
+      <div class='front'>
+      </div>
+      <div class='back rotated' style='background-color: ${color};'>
          <img src='${pokemon.sprites.front_default}' alt='${pokemon.name}'>
          <h2>${pokemon.name}</h2>
       </div>
     </div>
-    `
-  }).join('');
+    `;
+    })
+    .join('');
 
   game.innerHTML = pokemonHTML;
+};
 
-  
+const clickCard = (event) => {
+  const pokemonCard = event.currentTarget;
+
+
 
 }
+
+const getFrontAndBackFromCard = (card) => {
+  const front = card.querySelector(".front");
+  const back = card.querySelector(".back");
+  return [front, back]
+}
+
+const rotateElements = (elements) => {
+  if(typeof elements !== 'object' || !elements.length) return;
+  elements.forEach(element => element.classList.toggle('rotated'));
+}
+
 
 const resetGame = async () => {
-  const pokemon = await loadPokemom()
-  displayPokemon([...pokemon, ...pokemon])
-
-}
+  const pokemon = await loadPokemom();
+  displayPokemon([...pokemon, ...pokemon]);
+};
 
 resetGame();
